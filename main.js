f398e859e29cc9a1cf17b51c3ea99a828f03a4df
VidhanKansal1 <128469233+VidhanKansal1@users.noreply.github.com>
2023-06-13 12:09:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload(){
+}
+
+function setup(){
+    canvas = createCanvas(370,370);
+    canvas.center();
+}
+
+function draw(){
+}
+
+function take_snapshot(){
+    save("my_picture.png");
+}
